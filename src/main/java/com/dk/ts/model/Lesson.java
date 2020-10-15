bc74c559f40afbcfe67614cb7b604284370d610f
Troels Smidt <Razorsmidt@hotmail.com>
2020-10-15 15:16:48 +0200

    Had to fix lesson class, so program could boot

--- src/main/java/com/dk/ts/model/Lesson.java
+++ src/main/java/com/dk/ts/model/Lesson.java
@@ -3,8 +3,6 @@ package com.dk.ts.model;
 import javax.persistence.*;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "Lesson")
@@ -13,16 +11,11 @@ public class Lesson {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long lesson_id;
     private String description;
-    @ManyToMany(mappedBy = "fk_students")
-    private List<Student> studentList = new ArrayList<>();
-
+    private int fk_student;
     private int fk_teacher;
 
     public Lesson(String description){
         this.description = description;
-    }
-    public void addStudent(Student student){
-        studentList.add(student);
     }
 
 
